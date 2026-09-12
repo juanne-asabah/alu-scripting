bf04ecc7c76cc86b40e0a01d263411afe8415810
juanne-asabah <j.asabah@alustudent.com>
2026-09-12 00:21:37 -0700

    Correct repetition token 2

--- regular_expressions/3-repetition_token_2.rb
+++ regular_expressions/3-repetition_token_2.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
 #Check that an argument is passed and apply the Oniguruma regex
 if ARGV[0]
-    print ARGV[0].scan(/hbt{1,4}n/).join
+    print ARGV[0].scan(/hbt{1,5}n/).join
 end
